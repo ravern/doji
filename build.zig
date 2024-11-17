a7d605f75f96e8a30b69b1c89856fbdb9650e4b7
Ravern Koh <ravernkoh@gmail.com>
2024-11-17 23:22:43 +0800

    initial commit

---
+++ build.zig
@@ -0,0 +1,45 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const root_path = b.path("src/root.zig");
+    const main_path = b.path("src/main.zig");
+
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // --------------------- executable -----------------------
+
+    const lib = b.addStaticLibrary(.{
+        .name = "doji",
+        .root_source_file = root_path,
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(lib);
+
+    const exe = b.addExecutable(.{
+        .name = "doji",
+        .root_source_file = main_path,
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(exe);
+
+    const run_exe = b.addRunArtifact(exe);
+    run_exe.step.dependOn(&exe.step);
+
+    const run_step = b.step("run", "Run the executable");
+    run_step.dependOn(&run_exe.step);
+
+    // ----------------------- tests -------------------------
+
+    const tests = b.addTest(.{
+        .root_source_file = root_path,
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_tests = b.addRunArtifact(tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_tests.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+const doji = @import("root.zig");
+
+fn readLine(allocator: std.mem.Allocator, reader: anytype) ![]const u8 {
+    return reader.readUntilDelimiterAlloc(allocator, '\n', std.math.maxInt(usize));
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const in = std.io.getStdIn().reader();
+    const out = std.io.getStdOut().writer();
+
+    try out.writeAll("Dōji v0.0.0\n");
+    while (true) {
+        try out.writeAll("> ");
+
+        const line = readLine(allocator, in) catch |err| {
+            switch (err) {
+                error.EndOfStream => {
+                    try out.print("\nbye.\n", .{});
+                    return;
+                },
+                else => return err,
+            }
+        };
+        defer allocator.free(line);
+
+        try out.print("{s}\n", .{line});
+    }
+}
